101bca2cac2ed6df6812a7c4d1b2dbafb8990b2f
Eugene-Khonich <161977131+Eugene-Khonich@users.noreply.github.com>
2024-12-12 21:19:22 +0200

    routers/index

---
+++ src/db/models/user.js
@@ -0,0 +1,14 @@
+// src/db/models/user.js
+
+import { Schema, model } from 'mongoose';
+
+const usersSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const UsersCollection = model('users', usersSchema);
