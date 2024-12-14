2f4935873306b70d9a642b5e632e90a5b6f20008
Eugene-Khonich <ehonich@gmail.com>
2024-12-14 21:20:48 +0200

    1

--- src/server.js
+++ src/server.js
@@ -16,7 +16,6 @@ const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
-
   app.use(
     express.json({
       type: ['application/json', 'application/vnd.api+json'],
@@ -25,16 +24,12 @@ export const startServer = () => {
   );
   app.use(cors());
   app.use(logger);
-
   app.get('/', (req, res) => {
     res.json({ message: 'Hello world!' });
   });
-
   app.use(studentsRouter);
-
   app.use('*', notFoundHandler);
   app.use(errorHandler);
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
