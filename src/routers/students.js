4fdddb969c79243dd91480dc6705b3e7ad00d4e8
Eugene-Khonich <ehonich@gmail.com>
2024-12-22 17:57:49 +0200

    add full auth and roles

--- src/routers/students.js
+++ src/routers/students.js
@@ -17,25 +17,40 @@ import {
   patchStudentController,
 } from '../controllers/students.js';
 import { authenticate } from '../middlewares/authenticate.js';
+import { ROLES } from '../constans/index.js';
+import { checkRoles } from '../middlewares/checkRoles.js';
 
 const router = Router();
 
 router.use(authenticate);
-router.get('/', ctrlWrapper(getStudentsController));
-router.get('/:studentId', isValidId, ctrlWrapper(getStudentByIdController));
+router.get('/', checkRoles(ROLES.TEACHER), ctrlWrapper(getStudentsController));
+router.get(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
+  isValidId,
+  ctrlWrapper(getStudentByIdController),
+);
 router.post(
-  '/register',
+  '/',
+  checkRoles(ROLES.TEACHER),
   validateBody(createStudentSchema),
   ctrlWrapper(createStudentController),
 );
-router.delete('/:studentId', isValidId, ctrlWrapper(deleteStudentController));
+router.delete(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER),
+  isValidId,
+  ctrlWrapper(deleteStudentController),
+);
 router.put(
   '/:studentId',
+  checkRoles(ROLES.TEACHER),
   validateBody(createStudentSchema),
   ctrlWrapper(upsertStudentController),
 );
 router.patch(
   '/:studentId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
   validateBody(updateStudentSchema),
   ctrlWrapper(patchStudentController),
 );
--- src/validation/students.js
+++ src/validation/students.js
@@ -5,10 +5,11 @@ import Joi from 'joi';
 export const createStudentSchema = Joi.object({
   name: Joi.string().min(3).max(30),
   email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16),
+  age: Joi.number().integer().min(6).max(50),
   gender: Joi.string().valid('male', 'female', 'other'),
   avgMark: Joi.number().min(2).max(12),
   onDuty: Joi.boolean(),
+  parentId: Joi.string(),
 });
 
 export const updateStudentSchema = Joi.object({
